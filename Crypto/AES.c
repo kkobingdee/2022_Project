583d4e9caefec828443ef2bfbb8f6335b12a923b
SeungHyeon Jeon <99165076+kkobingdee@users.noreply.github.com>
2022-09-19 12:15:34 +0900

    Update AES.c

--- Crypto/AES.c
+++ Crypto/AES.c
@@ -43,8 +43,8 @@ u8 inv_Sbox[256] = {
 
 #define MUL2(a) (((a)<<1)^((a)&0x80?0x1b:0))
 #define MUL3(a) (MUL2(a))^(a)
-#define MUL4(a) MUL2((MUL2(a)))
-#define MUL8(a) MUL2((MUL2((MUL2(a)))))
+#define MUL4(a) (MUL2(MUL2(a)))
+#define MUL8(a) (MUL2(MUL2(MUL2(a))))
 #define MUL9(a) (MUL8(a))^(a)
 #define MULB(a) (MUL8(a))^(MUL2(a))^(a)
 #define MULD(a) (MUL8(a))^(MUL4(a))^(a)
